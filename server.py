9384391c89d1705e2a36c45b4218404ff0e200cb
Jeremy <jer.wang25@gmail.com>
2016-07-04 18:47:29 -0500

    UI makover to one-paged site

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+import urllib2, json
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html');
+
+@app.route('/education')
+def education():
+    return render_template('education.html');
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html');
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html');
+
+@app.route('/test')
+def test():
+    return render_template('test.html');
+
+if __name__ == "__main__":
+    app.run()
